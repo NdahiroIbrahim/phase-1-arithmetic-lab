6fd13f7037608f0609b9f4b310a8aaeb7e06dbdf
hassan <hassanibrahimndahiro@gmail.com>
2024-07-04 02:31:11 +0300

    initial

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+
+//multiplication
+
+let num1 = 10;
+let num2 = 6.2;
+const multiply = num1 * num2;
+console.log(multiply)
+
+//random
+
+const random = Math.floor(Math.random() * 10);
+console.log(random)
+
+//to calculate remainder equal to 4
+
+let num3 = 29;
+let num4 = 5;
+const mod = num3 % num4;
+console.log(mod)
+
+//max
+
+const max = Math.max(5,8,12,15,18,20)
+console.log(max)
+
